c829e66fb7c920414d2d145a72b26755a2e16e6a
Jack Platten <jack@platten.me>
2023-06-08 21:16:14 -0700

    Update .wezterm.lua

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -1,5 +1,5 @@
 -- Pull in the wezterm API
-local wezterm = require 'argonaut'
+local wezterm = require 'wezterm'
 
 -- This table will hold the configuration.
 local config = {}
@@ -13,7 +13,7 @@ end
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
-config.color_scheme = 'Monokai Remastered'
+config.color_scheme = 'Argonaut'
 
 config.font = wezterm.font('Berkeley Mono')
 -- config.harfbuzz_features = {'ss03'}
